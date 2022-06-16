2db27f794ec490ce481d0c87a37c9cd6ddcc3bae
Eugene <66912450+Eugeme@users.noreply.github.com>
2022-06-16 10:06:46 +0200

    Update and rename main.py to crawler.py

--- crawler.py
+++ crawler.py
@@ -3,25 +3,27 @@ from requests import get
 from openpyxl import load_workbook
 
 
+url = 'https://www.baustoffshop.de/catalogsearch/result/?q=1040bs'
+result = get(url)
+soup = BeautifulSoup(result.text, 'html.parser')
+urls = soup.find_all('a', class_="product-thumb-link")
+for i in range(len(urls)):
+    urls[i] = urls[i]['href']
+
+    
 def find_data(url):
     response = get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     ean = soup.find('div', class_="product attribute content_technicalfact_3")
     price = soup.find('div', class_="price-box").find('span', class_='price')
-    ean = ean.text.replace(' ', '').replace('\n', '')[4:]
+    ean = ean.text.ean.text.strip()[5:]
     price = price.text[:-2]
     return ean, price
 
 
-url = 'https://www.baustoffshop.de/catalog/product/view/id/'
-urls = ['']
-for i in range(1, 6):
-    urls.append(url + str(390114+i))
-    urls.append(url + str(482287+i))
-
 workbook = load_workbook('test_task_output_format.xlsx')
 worksheet = workbook.active
-for i in range(2, len(urls)+1):
-    worksheet['A' + str(i)].value = find_data(urls[i-1])[0]
-    worksheet['B' + str(i)].value = find_data(urls[i-1])[1]
+for i in range(len(urls)):
+    worksheet['A' + str(i)].value = find_data(urls[i])[0]
+    worksheet['B' + str(i)].value = find_data(urls[i])[1]
 workbook.save('test_task_output_format.xlsx')
